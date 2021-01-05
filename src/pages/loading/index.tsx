f34294103e41ac15c73612b268ebf1d1c05ba808
John Yi <john.yi1411@gmail.com>
2021-01-04 19:14:37 -0800

    Add animation icon.

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -1,11 +1,20 @@
 import React, { FunctionComponent } from 'react';
+import { IconContext } from "react-icons";
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 type LoadingProps = {
   className: string,
 };
 
 const Loading: FunctionComponent<LoadingProps> = ({ className }) => {
-  return <div className={`${className} fixed top-0 left-0 w-screen h-screen bg-lightgreen`}>whoa</div>
+  return (
+      <div className={`${className} fixed top-0 left-0 w-screen h-screen bg-lightgreen`}>
+        whoa
+        <IconContext.Provider value={{ className: "animate-spin text-6xl text-darkblue" }}>
+          <AiOutlineLoading3Quarters />
+        </IconContext.Provider>
+      </div>
+  );
 };
 
 export { Loading };
